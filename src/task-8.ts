7b5e66f12ea850c637380ea5a4b1ba2d11f81013
Andriy <admin@Mac.netis.cc>
2026-07-05 17:24:06 +0300

    fix

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,15 +1,17 @@
 import axios from "axios";
 
+
 interface Post {
-    id: number;
-    title: string;
-    body: string;
+  id: number;
+  title: string;
+  body: string;
 }
 
-const fetchPosts = async (): Promise<Post[]> => {
+async function fetchPosts(): Promise<Post[]> {
   const response = await axios.get<Post[]>(
-    'https://jsonplaceholder.typicode.com/posts'
+    "https://jsonplaceholder.typicode.com/posts"
   );
+
   return response.data;
 }
 
